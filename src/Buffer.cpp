7efa0c5ec904408218555d0fbcf16734107518ef
Purza Vlad <vpurza@gmail.com>
2025-10-24 15:11:04 +0300

    aw

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -1,40 +1,41 @@
 #include "Buffer.h"
 
-// Constructor cu inițializare
+// aloca memorie noua pentru un buffer de dim s (constructor)
 Buffer::Buffer(size_t s) : size(s), data(new char[s]) {
     std::cout << "Constructor: am alocat " << size << " bytes.\n";
     data[0] = '\0';
 }
 
-// Destructor
+// automat la final, pentru ca a fost initializat de tip buffer (destructor)
 Buffer::~Buffer() {
     std::cout << "Destructor: eliberez memoria (" << size << " bytes).\n";
     delete[] data;
 }
 
-// Copy constructor (copie profundă)
+// aloca memorie noua si copiaza (copy)
 Buffer::Buffer(const Buffer& other) : size(other.size), data(new char[other.size]) {
     std::memcpy(data, other.data, size);
     std::cout << "Copy constructor: copiez " << size << " bytes.\n";
 }
 
-// Move constructor
+// muta datele din vechiul buffer in unul nou (move)
 Buffer::Buffer(Buffer&& other) noexcept : size(other.size), data(other.data) {
     other.data = nullptr;
     other.size = 0;
     std::cout << "Move constructor: am mutat resursa.\n";
 }
 
-// Încapsulare - metode
+// pentru incapsulare
 void Buffer::setData(const char* text) {
     std::strncpy(data, text, size - 1);
     data[size - 1] = '\0';
 }
 
+//pentru incapsulare, pentru extract
 const char* Buffer::getData() const {
     return data;
 }
 
 void Buffer::print() const {
-    std::cout << "Buffer conține: " << data << "\n";
+    std::cout << "Buffer contine: " << data << "\n";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,24 @@
 
 Buffer createTemporaryBuffer() {
     Buffer temp(20);
-    temp.setData("Salut din move!");
+    temp.setData("in move");
     return temp; // declanșează move constructor
 }
 
 int main() {
-    std::cout << "=== Demonstrație tema ===\n";
 
     Buffer b1(15);
-    b1.setData("Salut lume");
+    b1.setData("test");
     b1.print();
 
-    std::cout << "\n--- Test Copy Constructor ---\n";
-    Buffer b2 = b1;  // declanșează copy constructor
+    std::cout << "\nCopy Constructor\n";
+    Buffer b2 = b1;  // declanseaza copy constructor
     b2.print();
 
-    std::cout << "\n--- Test Move Constructor ---\n";
-    Buffer b3 = createTemporaryBuffer();  // declanșează move constructor
+    std::cout << "\nMove Constructor\n";
+    Buffer b3 = createTemporaryBuffer();  // declanseaza move constructor
     b3.print();
 
-    std::cout << "\n--- Sfârșit program ---\n";
+    std::cout << "\nSfarsit\n";
     return 0;
 }
